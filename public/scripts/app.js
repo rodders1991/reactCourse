f5951fcffcd16254d8d1af622afb1bd9b8aca12f
Emma Webb <emmawebb@Emmas-MacBook-Pro.local>
2018-07-05 19:42:45 -0500

    Section 3, Lecture 16

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,44 +1,91 @@
 'use strict';
 
-var add = function add(a, b) {
-	return a + b;
+console.log('App.js is running!');
+
+// JSX - JavaScript XML
+var app = {
+  title: 'Indecision App',
+  subTitle: 'This is some info',
+  options: ['one', 'two']
 };
 
-console.log(add(55, 1));
-
-// This keyword
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subTitle && React.createElement(
+    'p',
+    null,
+    app.subTitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? "Here are your options: " : "No options"
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
+);
 
 var user = {
-	name: 'George',
-	cities: ['London', 'Hong Kong', 'Cardiff'],
-	printPlacesLived: function printPlacesLived() {
-		var _this = this;
-
-		console.log(this.name);
-		console.log(this.cities);
-
-		var cityMessages = this.cities.map(function (city) {
-			return _this.name + ' has lived in ' + city;
-		});
-
-		return cityMessages;
-	}
+  age: 17,
+  location: 'London'
 };
 
-console.log(user.printPlacesLived());
+var count = 0;
 
-// Challenge area
-
-var multiplier = {
-	numbers: [5, 6, 8],
-	multiplyBy: 2,
-	multiply: function multiply() {
-		var _this2 = this;
-
-		return this.numbers.map(function (n) {
-			return n * _this2.multiplyBy;
-		});
-	}
+var addOne = function addOne() {
+  return console.log('addOne');
+};
+var minusOne = function minusOne() {
+  return console.log('minusOne');
+};
+var reset = function reset() {
+  return console.log('reset');
 };
 
-console.log(multiplier.multiply());
+var template2 = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Count: ',
+    count
+  ),
+  React.createElement(
+    'button',
+    { onClick: addOne },
+    '+1'
+  ),
+  React.createElement(
+    'button',
+    { onClick: minusOne },
+    '-1'
+  ),
+  React.createElement(
+    'button',
+    { onClick: reset },
+    'reset'
+  )
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template2, appRoot);
--- src/app.js
+++ src/app.js
@@ -24,14 +24,21 @@ const user = {
   location: 'London'
 };
 
+let count = 0;
+
+const addOne = () => console.log('addOne');
+const minusOne = () => console.log('minusOne');
+const reset = () => console.log('reset');
+
 const template2 = (
   <div>
-    <h1>{user.name ? user.name : 'Anonymous'}</h1>
-    {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-    <p>Location: {user.location}</p>
+    <h1>Count: {count}</h1>
+    <button onClick={addOne}>+1</button>
+    <button onClick={minusOne}>-1</button>
+    <button onClick={reset}>reset</button>
   </div>
 );
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
