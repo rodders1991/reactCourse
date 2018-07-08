7837c36f05c796806c7ca1292796510d9793d819
George <george@everledger.io>
2018-07-08 23:01:23 +0100

    Section 4, part 1

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,37 +1,37 @@
 'use strict';
 
-console.log('App.js is running!');
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var appRoot = document.getElementById('app');
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var visibility = false;
-var toggleDetails = function toggleDetails() {
-	visibility = !visibility;
-	render();
-};
+var Person = function () {
+  function Person() {
+    var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
+    var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
 
-var render = function render() {
-	var template = React.createElement(
-		'div',
-		null,
-		React.createElement(
-			'h1',
-			null,
-			'Visibility Toggle'
-		),
-		React.createElement(
-			'button',
-			{ onClick: toggleDetails },
-			visibility ? 'Hide Details' : 'Show details'
-		),
-		visibility && React.createElement(
-			'p',
-			null,
-			'Hi how are you?'
-		)
-	);
+    _classCallCheck(this, Person);
 
-	ReactDOM.render(template, appRoot);
-};
+    this.name = name;
+    this.age = age;
+  }
 
-render();
+  _createClass(Person, [{
+    key: 'getGretting',
+    value: function getGretting() {
+      return 'Hi, I am ' + this.name + '!';
+    }
+  }, {
+    key: 'getDescription',
+    value: function getDescription() {
+      return this.name + ' is ' + this.age + ' year(s) old';
+    }
+  }]);
+
+  return Person;
+}();
+
+var me = new Person('George Ormrod', 26);
+console.log(me.getDescription());
+
+var other = new Person();
+console.log(other.getDescription());
