661e89ff6545eda8d8bbfce44747c989758a1218
George <george@everledger.io>
2018-07-09 15:13:15 +0100

    Section 4, part 28

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -20,12 +20,15 @@ var IndecisionApp = function (_React$Component) {
   _createClass(IndecisionApp, [{
     key: 'render',
     value: function render() {
+      var title = 'Indecision';
+      var subTitle = 'Put your life in the hands of a computer';
+      var options = ['Thing 1', 'thing 2', 'thing 3', 'thing 4'];
       return React.createElement(
         'div',
         null,
-        React.createElement(Header, null),
+        React.createElement(Header, { title: title, subTitle: subTitle }),
         React.createElement(Action, null),
-        React.createElement(Options, null),
+        React.createElement(Options, { options: options }),
         React.createElement(AddOption, null)
       );
     }
@@ -52,12 +55,12 @@ var Header = function (_React$Component2) {
         React.createElement(
           'h1',
           null,
-          'Indecision'
+          this.props.title
         ),
         React.createElement(
           'h2',
           null,
-          'Put your life in the hands of a computer'
+          this.props.subTitle
         )
       );
     }
@@ -108,12 +111,9 @@ var Options = function (_React$Component4) {
       return React.createElement(
         'div',
         null,
-        React.createElement(
-          'p',
-          null,
-          'Options component here'
-        ),
-        React.createElement(Option, null)
+        this.props.options.map(function (option) {
+          return React.createElement(Option, { key: option, optionText: option });
+        })
       );
     }
   }]);
@@ -139,7 +139,7 @@ var Option = function (_React$Component5) {
         React.createElement(
           'p',
           null,
-          'Option component here'
+          this.props.optionText
         )
       );
     }
--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 class IndecisionApp extends React.Component {
   render() {
+    const title = 'Indecision';
+    const subTitle = 'Put your life in the hands of a computer';
+    const options = ['Thing 1', 'thing 2', 'thing 3', 'thing 4'];
     return (
       <div>
-        <Header />
+        <Header title={title} subTitle={subTitle}/>
         <Action />
-        <Options />
+        <Options options={options}/>
         <AddOption />
       </div>
     );
@@ -14,8 +17,8 @@ class Header extends React.Component {
   render() {
     return (
       <div>
-        <h1>Indecision</h1>
-        <h2>Put your life in the hands of a computer</h2>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.subTitle}</h2>
       </div>
     );
   }
@@ -35,8 +38,9 @@ class Options extends React.Component {
   render() {
     return (
       <div>
-        <p>Options component here</p>
-        <Option />
+        {
+          this.props.options.map( option => <Option key={option} optionText={option} />)
+        }
       </div>
     );
   }
@@ -46,7 +50,7 @@ class Option extends React.Component {
     render() {
       return (
         <div>
-          <p>Option component here</p>
+          <p>{this.props.optionText}</p>
         </div>
       )
     }
