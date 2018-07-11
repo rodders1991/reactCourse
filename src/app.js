f60d0d8d9985750aca5979a99762929996546696
George <george@everledger.io>
2018-07-11 15:49:12 +0100

    Section 6, part 53

--- src/app.js
+++ src/app.js
@@ -1,16 +1,6 @@
-import substract, { square, add } from './utils';
-import isSenior, { isAdult, canDrink } from './person';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('App.js is running');
+const template = React.createElement('p', {}, 'Testing 123');
 
-// console.log(square(4));
-//
-// console.log(add(2,2));
-//
-// console.log(substract(50, 10));
-
-// console.log(isAdult(26));
-//
-// console.log(canDrink(18));
-
-console.log(isSenior(64));
+ReactDOM.render(template, document.getElementById('app'));
