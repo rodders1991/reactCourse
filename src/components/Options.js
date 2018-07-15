69e69f2db4d45b541beb845c63e09928e5bb4597
George <george@everledger.io>
2018-07-15 19:11:40 +0100

    Section 8, part 70

--- src/components/Options.js
+++ src/components/Options.js
@@ -9,10 +9,11 @@ const Options = props => (
     </div>
     {props.options.length === 0 && <p className="widget__message">Please add a option to get started</p>}
     {
-      props.options.map( option => (
+      props.options.map( (option, index) => (
         <Option
           key={option}
           optionText={option}
+          count={index + 1}
           handleDeleteOption={props.handleDeleteOption}
         />
     ))
