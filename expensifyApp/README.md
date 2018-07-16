bf234ab0dc5798acc2984791de1d13bc222902d8
George <george@everledger.io>
2018-07-16 17:36:02 +0100

    Created boilerplate app and second project structure

---
+++ expensifyApp/README.md
@@ -0,0 +1,12 @@
+# Indecision App
+
+## Prerequisites
+
+- Please install node and npm (https://nodejs.org/en/)
+- Please install yarn with the command `npm i -g yarn`
+
+## Dev deployment
+
+- Run `yarn` to install external modules
+- Run `yarn run dev-server`
+- The Indecision app should now be running on localhost:8080
---
+++ expensifyApp/webpack.config.js
@@ -0,0 +1,30 @@
+// entry -> output
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      // Regular expression to include all .js files
+      test: /\.js$/,
+      exclude: /node_modules/
+    }, {
+      test: /\.s?css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    }
+  ]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
+};
